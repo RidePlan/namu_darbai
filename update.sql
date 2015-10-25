b2ad5b21e6e6acf5b14e4724810754d7b09b9b04
Kęstutis Triponis <kestutis.triponis@gmail.com>
2015-10-25 21:45:49 +0200

    Add database update script
    The script performs every task and transforms the original database into a final version.

---
+++ update.sql
@@ -0,0 +1,31 @@
+ALTER TABLE Authors MODIFY name VARCHAR(255) CHARACTER SET cp1257 collate cp1257_lithuanian_ci NOT NULL;
+
+INSERT INTO Authors (name) VALUES ('Ričardas Jonaitis');
+INSERT INTO Authors (name) VALUES ('Kęstutis Triponis');
+INSERT INTO Authors (name) VALUES ('Ramūnas Liuokaitis');
+
+INSERT INTO Books (authorId, title, year) VALUES (8, "Mano gyvenimas", "2015");
+INSERT INTO Books (authorId, title, year) VALUES (9, "Tobulas pasaulis", "2015");
+INSERT INTO Books (authorId, title, year) VALUES (10, "Mokausi", "2015");
+
+UPDATE Books SET AuthorId = 8 WHERE bookid = 7;
+
+DELETE FROM Authors WHERE authorId = 8 OR authorId = 9 OR authorId = 10;
+
+DELETE b FROM Books b LEFT JOIN Authors a ON b.authorId = a.authorId WHERE a.authorId IS NULL;
+
+ALTER TABLE Books ADD COLUMN genre VARCHAR(50) NOT NULL;
+
+CREATE TABLE Tarpine(authorId int(11) default null, bookId int(11) default null);
+
+insert into Tarpine(authorId, bookId) select authorId, bookId from Books;
+
+ALTER TABLE Books DROP COLUMN authorId;
+
+INSERT INTO Tarpine (authorId, bookId) values (3, 2);
+INSERT INTO Tarpine (authorId, bookId) values (5, 2);
+INSERT INTO Tarpine (authorId, bookId) values (5, 5);
+
+ALTER TABLE Books MODIFY TITLE VARCHAR(255) CHARACTER SET cp1257 collate cp1257_lithuanian_ci;
+UPDATE Books SET TITLE = "Head First Networking (ąčęėįšųūž)" WHERE bookId = 3;
+
